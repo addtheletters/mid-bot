d542a274a6d32950553374902fc8064065e14d0f
addtheletters <addtheletters@users.noreply.github.com>
2020-07-01 18:13:55 -0700

    ProcessPool to terminate/restart command workers
    ProcessPool provided by Pebble terminates processes if they exceed timeout.
    Client.run is overridden to prevent the event loop from self-stopping upon detecting subprocess termination.
    Prints are switch to logging.

--- py/src/main.py
+++ py/src/main.py
@@ -2,11 +2,16 @@
 from commands import *
 from config import *
 from dotenv import load_dotenv
-from utils import reply, print_message
+from pebble import ProcessPool
+from utils import reply, log_message
 import asyncio
 import concurrent.futures
 import discord
+import logging
 import os
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -14,39 +19,97 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 def help_notice():
     return f"See `{BOT_SUMMON_PREFIX}{DEFAULT_HELP_KEY}`."
 
+class PebbleExecutor(concurrent.futures.Executor):
+    def __init__(self, pebble_pool, timeout=None):
+        self.pool = pebble_pool
+        self.timeout = timeout
+
+    def submit(self, fn, *args, **kwargs):
+        return self.pool.schedule(fn, args=args, timeout=self.timeout)
+
+    def map(self, func, *iterables, timeout=None, chunksize=1):
+        return NotImplementedError("This wrapper does not yet support `map`.")
+
+    def shutdown(self, wait=True):
+        if wait:
+            log.info("Closing workers...")
+            self.pool.close()
+        else:
+            log.info("Ending workers...")
+            self.pool.stop()
+        self.pool.join()
+        log.info("Workers joined.")
+
 class MidClient(discord.Client):
     def __init__(self):
         discord.Client.__init__(self)
-        self.executor = concurrent.futures.ProcessPoolExecutor(max_workers=MAX_COMMAND_WORKERS)
+        self.executor = PebbleExecutor(
+            ProcessPool(max_workers=MAX_COMMAND_WORKERS),
+            COMMAND_TIMEOUT)
 
         self.commands = {}
         for cmd in COMMAND_CONFIG:
             for key in cmd.keys:
                 self.register_command(key, cmd)
 
+    # Override. Functionally near-identical to discord.Client.
+    # Prevent main loop from exiting on subprocess SIGINT/SIGTERM.
+    # Clean up executor workers upon completion.
+    def run(self, *args, **kwargs):
+        loop = self.loop
+        async def runner():
+            try:
+                await self.start(*args, **kwargs)
+            finally:
+                await self.close()
+
+        def stop_loop_on_completion(f):
+            loop.stop()
+
+        future = asyncio.ensure_future(runner(), loop=loop)
+        future.add_done_callback(stop_loop_on_completion)
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            log.info("Received signal to terminate bot and event loop.")
+        finally:
+            future.remove_done_callback(stop_loop_on_completion)
+            log.info("Cleaning up tasks.")
+            discord.client._cleanup_loop(loop)
+
+        if not future.cancelled():
+            return future.result()
+        self.executor.shutdown(False)
+
     def register_command(self, key, cmd):
         if key in self.commands.keys():
-            print(f"Key {key} is overloaded. Fix the command configuration.")
+            log.warning(f"Key {key} is overloaded. Fix the command configuration.")
         self.commands[key] = cmd
         if key not in cmd.keys:
             cmd.keys.append(key)
 
     async def execute_command(self, command_key, msg, intext):
-        event_loop = self.loop
-        cmd_future = event_loop.run_in_executor(self.executor, self.commands[command_key].func, intext)
+        #cmd_future = self.pool.schedule(self.commands[command_key].func, args=[intext], timeout=COMMAND_TIMEOUT)
+        cmd_future = self.loop.run_in_executor(self.executor, self.commands[command_key].func, intext)
         try:
+            #response = cmd_future.result()
             response = await asyncio.wait_for(cmd_future, timeout=COMMAND_TIMEOUT)
             return response
-        except asyncio.TimeoutError:
-            print(f"Command {command_key} timed out on input: {intext}. Resetting executor.")
+        except concurrent.futures.TimeoutError:
+            log.info(f"Command {command_key} timed out on input: {intext}.")
+            cmd_future.cancel()
+            raise
+        except Exception as err:
+            log.info(f"Command execution raised error: {err}")
+            cmd_future.cancel()
             raise
 
     async def on_ready(self):
-        print(f"{self.user} is now connected to Discord in guilds: {[g.name for g in self.guilds]}")
+        log.info(f"{self.user} is now connected to Discord in guilds: {[g.name for g in self.guilds]}")
 
     async def on_message(self, msg):
-        print_message(msg)
         if (self.should_process_message(msg)):
+            log_message(msg)
             await self.process_message(msg)
 
     def should_process_message(self, msg):
@@ -61,7 +124,7 @@ class MidClient(discord.Client):
 
     async def process_message(self, msg):
         if msg.channel == None:
-            print("Missing channel, can't reply.")
+            log.info("Missing channel, can't reply.")
             return
 
         async with msg.channel.typing():
@@ -86,7 +149,7 @@ class MidClient(discord.Client):
                         await reply(msg, command_response)
                     return
                 except concurrent.futures.TimeoutError:
-                    await reply(msg, f"Command execution timed out for `{command}`.")
+                    await reply(msg, f"Command execution timed out for `{command} {intext}`.")
             else:
                 await reply(msg, f"Unrecognized command `{command}`. {help_notice()}")
 
--- py/src/utils.py
+++ py/src/utils.py
@@ -1,14 +1,17 @@
 # Utility functions.
 import discord
+import logging
 from config import MAX_MESSAGE_LENGTH
+
+log = logging.getLogger(__name__)
 
 # Escape discord formatting
 def escape(text):
     return discord.utils.escape_markdown(text)
 
 # Log message contents
-def print_message(msg):
-    print(f"({msg.id}) {msg.created_at.isoformat(timespec='milliseconds')} [{msg.channel}] <{msg.author}> {msg.content}")
+def log_message(msg):
+    log.info(f"({msg.id}) {msg.created_at.isoformat(timespec='milliseconds')} [{msg.channel}] <{msg.author}> {msg.content}")
 
 # Send `text` in response to `msg`.
 async def reply(msg, text):
