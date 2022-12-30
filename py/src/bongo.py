16ec4d74c1a0543556f34954595f69a8f81baf0b
addtheletters <addtheletters@users.noreply.github.com>
2022-12-29 21:34:12 -0500

    more bongo animal APIs
    now available:
    random.cat
    random-d.uk
    randomfox.ca
    dog.ceo
    bunnies.io
    shibe.online
    some-random-api.ml/animal

--- py/src/bongo.py
+++ py/src/bongo.py
@@ -1,7 +1,6 @@
 # Commands for fetching random images from external APIs.
 # The name comes from its inspiration, a bot known as Böngo Cat.
 import logging
-from typing import Tuple
 
 import aiohttp
 import discord
@@ -12,8 +11,25 @@ log = logging.getLogger(__name__)
 
 
 RANDOM_CAT_ENDPOINT = "https://aws.random.cat/meow"
+RANDOM_DUCK_ENDPOINT = "https://random-d.uk/api/v2/random"
+RANDOM_FOX_ENDPOINT = "https://randomfox.ca/floof/"
+DOG_CEO_ENDPOINT = "https://dog.ceo/api/breeds/image/random"
+BUNNIES_IO_ENDPOINT = "https://api.bunnies.io/v2/loop/random/?media=gif"
+SHIBE_ONLINE_ENDPOINT = "https://shibe.online/api/shibes?count=1"
 SRA_PREFIX = "https://some-random-api.ml/"
 SRA_BIRD_ENDPOINT = SRA_PREFIX + "animal/bird"
+
+SRA_ANIMALS = [
+    "bird",
+    "cat",
+    "dog",
+    "fox",
+    "kangaroo",
+    "koala",
+    "panda",
+    "raccoon",
+    "red_panda",
+]
 
 
 def image_embed(
@@ -30,7 +46,7 @@ def bad_status(status: int):
     return status < 200 or status > 299
 
 
-# Returns json as dict, raises ConnectionError if unsuccessful
+# Returns json as dict, raises ConnectionError if unsuccessful.
 async def request_json(endpoint: str) -> dict:
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as response:
@@ -40,9 +56,34 @@ async def request_json(endpoint: str) -> dict:
             return json
 
 
-async def fetch_api_file(title: str, endpoint: str) -> discord.Embed:
-    re_json = await request_json(endpoint)
+async def fetch_random_cat(title: str) -> discord.Embed:
+    re_json = await request_json(RANDOM_CAT_ENDPOINT)
     return image_embed(title=title, img_url=re_json["file"])
+
+
+async def fetch_random_duck(title: str) -> discord.Embed:
+    re_json = await request_json(RANDOM_DUCK_ENDPOINT)
+    return image_embed(title=title, img_url=re_json["url"])
+
+
+async def fetch_random_fox(title: str) -> discord.Embed:
+    re_json = await request_json(RANDOM_FOX_ENDPOINT)
+    return image_embed(title=title, img_url=re_json["image"], link=re_json["link"])
+
+
+async def fetch_dog_ceo(title: str) -> discord.Embed:
+    re_json = await request_json(DOG_CEO_ENDPOINT)
+    return image_embed(title=title, img_url=re_json["message"])
+
+
+async def fetch_bunnies_io(title: str) -> discord.Embed:
+    re_json = await request_json(BUNNIES_IO_ENDPOINT)
+    return image_embed(title=title, img_url=re_json["media"]["gif"])
+
+
+async def fetch_shibe_online(title: str) -> discord.Embed:
+    re_json = await request_json(SHIBE_ONLINE_ENDPOINT)
+    return image_embed(title=title, img_url=re_json[0])
 
 
 async def fetch_some_random(title: str, endpoint: str) -> discord.Embed:
@@ -66,7 +107,37 @@ async def bongo(ctx):
 
 @bongo.command()
 async def cat(ctx: commands.Context):
-    embed = await fetch_api_file("Cat.", RANDOM_CAT_ENDPOINT)
+    embed = await fetch_random_cat("Cat.")
+    await reply(ctx, embed=embed)
+
+
+@bongo.command()
+async def duck(ctx: commands.Context):
+    embed = await fetch_random_duck("Quack.")
+    await reply(ctx, embed=embed)
+
+
+@bongo.command()
+async def fox(ctx: commands.Context):
+    embed = await fetch_random_fox("fox")
+    await reply(ctx, embed=embed)
+
+
+@bongo.command()
+async def dog(ctx: commands.Context):
+    embed = await fetch_dog_ceo("Dog...")
+    await reply(ctx, embed=embed)
+
+
+@bongo.command()
+async def bunny(ctx: commands.Context):
+    embed = await fetch_bunnies_io("bun")
+    await reply(ctx, embed=embed)
+
+
+@bongo.command()
+async def shibe(ctx: commands.Context):
+    embed = await fetch_shibe_online("shibe is a meme word for shiba inu")
     await reply(ctx, embed=embed)
 
 
@@ -77,6 +148,23 @@ async def bird(ctx: commands.Context):
 
 
 @bongo.command()
-async def sra(ctx: commands.Context, path: str):
-    embed = await fetch_some_random("Some Random API", SRA_PREFIX + path)
+async def sranimal(ctx: commands.Context, animal: str):
+    if animal not in SRA_ANIMALS:
+        await reply(ctx, "Sorry, the API doesn't have that animal.")
+        return
+    embed = await fetch_some_random(
+        f"Some Random API: {animal}", SRA_PREFIX + "animal/" + animal
+    )
     await reply(ctx, embed=embed)
+
+
+@sranimal.autocomplete("animal")
+async def sranimal_autocomplete(
+    interaction: discord.Interaction,
+    current: str,
+):
+    return [
+        discord.app_commands.Choice(name=animal, value=animal)
+        for animal in SRA_ANIMALS
+        if current.lower() in animal.lower()
+    ]
